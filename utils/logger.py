d78bd8fa5e1e685d482af256d5ecf4d4b8552061
whoami <diogo.m.lages@gmail.com>
2025-03-09 17:24:34 +0000

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -10,29 +10,23 @@ class ScannerLogger:
         self.setup_logger(config_path)
 
     def setup_logger(self, config_path):
-        # Load config
         with open(config_path) as f:
             config = yaml.safe_load(f)
 
         log_config = config["logging"]
 
-        # Create logs directory if it doesn't exist
         os.makedirs("logs", exist_ok=True)
 
-        # Configure logger
         self.logger.setLevel(logging.getLevelName(log_config["level"]))
 
-        # File handler with rotation
         file_handler = RotatingFileHandler(
             f"logs/{log_config['file']}",
             maxBytes=log_config["max_size"],
             backupCount=log_config["backup_count"]
         )
 
-        # Console handler
         console_handler = logging.StreamHandler()
 
-        # Formatter
         formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         )
